48ff2ce2324b620e9765e74f8ae6c9c3eabd1e54
Ian Casper <ian_casper@hotmail.com>
2019-05-14 16:50:13 +0200

    Updated index.html and users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 res.set('content-type', 'application/json');
-res.json({firstName: 'Ian', lastName: 'Casper'});
+res.json({firstName: 'Ian', lastName: 'Casper', address: {street: '13 Gold Circle', suburb: 'Lyttelton Manor', town: 'Centurion', postalCode: '0157'}});
 });
 
 module.exports = router;
